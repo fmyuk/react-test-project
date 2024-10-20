05bc6bc3886b59a182ebcfe307f4ff6391039aee
fumi uk <dcfumiya.238@gmail.com>
2024-10-20 11:30:04 +0900

    react test project

---
+++ src/components/greet/Greet.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type GreetProps = {
+  name?: string;
+};
+
+const Greet = ({ name }: GreetProps) => {
+  return <div>Hello {name ? name : "Guest"}</div>;
+};
+
+export default Greet;
