3f472b572b08fce48da6641175f2c866f53e74ee
fumi uk <dcfumiya.238@gmail.com>
2024-10-21 18:12:19 +0900

    react test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Contact from "./components/contact/Contact";
+import Counter from "./components/counter/Counter";
 
 function App() {
   return (
     <div className="App">
       <Contact />
+      <Counter />
     </div>
   );
 }
